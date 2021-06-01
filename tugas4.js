565748c4e83f2a5f2b0139f152d05e6c2f0fda6c
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:43:29 +0700

    Initial commit

---
+++ tugas4.js
@@ -0,0 +1,26 @@
+var budi = 160;
+var bayu = 170;
+var rudi = 165;
+
+if (bayu > budi) {
+  console.log('bayu dengan tinggi pertama');
+} else if (bayu > rudi) {
+  console.log('bayu dengan tinggi kedua');
+} else {
+  console.log('bayu dengan tinggi ketiga');
+}
+
+if (rudi > bayu) {
+  console.log('rudi dengan tinggi pertama');
+} else if (rudi > budi) {
+  console.log('rudi dengan tinggi kedua');
+} else {
+  console.log('rudi dengan tinggi ketiga');
+}
+if (budi > bayu) {
+  console.log('budi dengan tinggi pertama');
+} else if (budi > rudi) {
+  console.log('budi dengan tinggi kedua');
+} else {
+  console.log('budi dengan tinggi ketiga');
+}
